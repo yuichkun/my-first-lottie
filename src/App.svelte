49fa58821e18c2a4c4ef052609ffcb421c8da8d6
yuichkun <yuichkun@gmail.com>
2023-05-12 18:50:26 +0900

    enable replacing files

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,18 @@
   import { onMount } from "svelte";
   let element: HTMLDivElement;
 
+  function render(jsonStr: string) {
+    const animationData = JSON.parse(jsonStr);
+    lottie.destroy();
+    lottie.loadAnimation({
+      container: element, // the dom element that will contain the animation
+      renderer: "svg",
+      loop: true,
+      autoplay: true,
+      animationData,
+    });
+  }
+
   onMount(() => {
     lottie.loadAnimation({
       container: element, // the dom element that will contain the animation
@@ -12,11 +24,21 @@
       path: "lottie.json", // the path to the animation json
     });
   });
+  const onChange = (e: any) => {
+    const [file] = e.target.files;
+    if (!file) alert("specify file!!");
+    const reader = new FileReader();
+    reader.onload = function () {
+      const lottieFile = reader.result;
+      render(lottieFile as string);
+    };
+    reader.readAsText(file);
+  };
 </script>
 
 <main>
-  <h1>Vite + Svelte</h1>
   <div id="container" bind:this={element} />
+  <input type="file" id="load" on:change={onChange} />
 </main>
 
 <style>
@@ -25,12 +47,17 @@
     min-height: 300px; */
     max-width: 100%;
     max-height: 100%;
-    background-color: #1e1c26;
     position: absolute;
     top: 0;
     left: 0;
     right: 0;
     bottom: 0;
     margin: auto;
+  }
+  #load {
+    position: absolute;
+    top: 0;
+    left: 0;
+    border: 1px solid white;
   }
 </style>
